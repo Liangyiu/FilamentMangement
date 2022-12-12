e2f081720324a7c51cba926c36a2bb89b03844bd
Marcel <45638961+Liangyiu@users.noreply.github.com>
2022-12-12 15:54:03 +0100

    added new producer functionality

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,14 +2,65 @@ import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components/ui
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import NfcManager, { NfcEvents, NfcTech } from 'react-native-nfc-manager';
+import Prompt from '../components/Prompt';
 
 function Home({ navigation }) {
+    const promptRef = React.useRef();
+    let tag = undefined;
+
+    async function readTagId() {
+        try {
+            await NfcManager.requestTechnology(NfcTech.Ndef);
+            const tag = await NfcManager.getTag();
+
+            return tag.id;
+        } catch (ex) {
+            console.warn('Oops!', ex);
+        } finally {
+            NfcManager.cancelTechnologyRequest();
+        }
+    }
+
+    async function scanTag(option = 'none') {
+        if (Platform.OS === 'android') {
+            promptRef.current.setVisible(true);
+        }
+
+        tag = await readTagId();
+
+        if (Platform.OS === 'android') {
+            promptRef.current.setVisible(false);
+        }
+
+        if (tag && option === 'info') {
+            navigation.navigate('TagInfo', { tagId: tag });
+        }
+    }
+
     return (
         <>
             <TopNavigation title="Home" alignment="center" />
             <Divider />
             <Layout level="4" style={styles.wrapper}>
-                <Button onPress={() => navigation.navigate('Info')}>Show Info</Button>
+                <Button
+                    onPress={() => {
+                        scanTag('info');
+                    }}>
+                    Show Info
+                </Button>
+                <Button
+                    onPress={() => {
+                        navigation.navigate('NewProducer');
+                    }}>
+                    Add a new Filament-Producer
+                </Button>
+                <Prompt
+                    ref={promptRef}
+                    onCancelPress={() => {
+                        NfcManager.cancelTechnologyRequest();
+                    }}
+                />
             </Layout>
         </>
     );
